ac360733dc076e55d74934775c13cd8d1ce6736e
Miguel Antezana <antezana021@gmail.com>
2026-07-21 12:36:03 -0400

    Volver a orderService con UUID

--- services/orderService.js
+++ services/orderService.js
@@ -1,82 +1,28 @@
-const SHOP = process.env.SHOPIFY_STORE;
-const TOKEN = process.env.SHOPIFY_ADMIN_ACCESS_TOKEN;
-const VERSION = process.env.SHOPIFY_API_VERSION;
+const { v4: uuidv4 } = require("uuid");
+
+// Memoria temporal de pedidos
+const orders = {};
 
 /**
- * Crear una orden en Shopify
+ * Crear pedido
  */
-async function createOrder(orderData) {
+function createOrder(orderData) {
 
-    const line_items = [];
+    const orderId = uuidv4();
 
-    if (orderData.items) {
+    orders[orderId] = orderData;
 
-        for (const item of orderData.items) {
+    return orderId;
 
-            line_items.push({
-                variant_id: item.variant_id,
-                quantity: item.quantity
-            });
-
-        }
-
-    }
-
-    const response = await fetch(
-        `https://${SHOP}/admin/api/${VERSION}/orders.json`,
-        {
-            method: "POST",
-            headers: {
-                "X-Shopify-Access-Token": TOKEN,
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                order: {
-                    line_items,
-                    financial_status: "pending"
-                }
-            })
-        }
-    );
-
-    const data = await response.json();
-
-    console.log("========== SHOPIFY ==========");
-    console.log("Status:", response.status);
-    console.log(JSON.stringify(data, null, 2));
-    console.log("=============================");
-
-    if (!response.ok) {
-        throw new Error(JSON.stringify(data));
-    }
-
-    if (!data.order) {
-        throw new Error("Shopify no devolvió una orden.");
-    }
-
-    return data.order.id;
 }
 
 /**
- * Obtener una orden
+ * Obtener pedido
  */
-async function getOrder(orderId) {
+function getOrder(orderId) {
 
-    const response = await fetch(
-        `https://${SHOP}/admin/api/${VERSION}/orders/${orderId}.json`,
-        {
-            headers: {
-                "X-Shopify-Access-Token": TOKEN
-            }
-        }
-    );
+    return orders[orderId];
 
-    const data = await response.json();
-
-    console.log("GET ORDER:");
-    console.log(JSON.stringify(data, null, 2));
-
-    return data.order;
 }
 
 module.exports = {
