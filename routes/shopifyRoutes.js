f55e5576392e9ea27c7cdb8729fb09829eb79d87
Miguel Antezana <antezana021@gmail.com>
2026-07-31 20:38:11 -0400

    Corrección: Importar getOrderDetails en las rutas

--- routes/shopifyRoutes.js
+++ routes/shopifyRoutes.js
@@ -1,3 +1,4 @@
+// C:\Users\PC MIGUEL\Desktop\tecnopolis\backend\routes\shopifyRoutes.js
 const express = require("express");
 const router = express.Router();
 
@@ -5,13 +6,14 @@ const {
     testConnection,
     listProducts,
     createOrder,
-    exchangeRate
+    exchangeRate,
+    getOrderDetails // <--- ¡Asegúrate de que esta línea esté aquí!
 } = require("../controllers/shopifyController");
 
 router.get("/test", testConnection);
 router.get("/products", listProducts);
 router.post("/create-order", createOrder);
 router.get("/exchange-rate", exchangeRate);
-router.get("/order/:orderId", getOrderDetails);
+router.get("/order/:orderId", getOrderDetails); // <--- Y esta ruta activa
 
 module.exports = router;
